2b828eb221c6d5b3c076128a2f723d71cbed55cf
mahsadehghanmanshadi <79363734+MahsaDehghan@users.noreply.github.com>
2024-05-20 10:19:39 +0330

    utils.py

--- utils.py
+++ utils.py
@@ -148,7 +148,7 @@ def infer_uploaded_webcam(conf, model):
         flag = st.button(
             label="Stop running"
         )
-        vid_cap = cv2.VideoCapture(0)  # local camera
+        vid_cap = cv2.VideoCapture(1)  # local camera
         st_frame = st.empty()
         while not flag:
             success, image = vid_cap.read()
